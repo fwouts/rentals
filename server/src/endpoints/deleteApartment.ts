c6a7254322d1fe3a19006b7e1b3892618ffa1c57
Francois Wouts <f@zenc.io>
2018-04-04 15:07:15 +1000

    Set up apartment listing deletion

--- server/src/endpoints/deleteApartment.ts
+++ server/src/endpoints/deleteApartment.ts
@@ -1,11 +1,56 @@
+import { decodeJwt } from "@/auth/jwt";
+import { connection } from "@/db/connections";
+import { Apartment } from "@/db/entities/apartment";
 import { AuthRequired, DeleteApartmentResponse } from "../api";
 
 export async function deleteApartment(
   headers: AuthRequired,
   apartmentId: string,
 ): Promise<DeleteApartmentResponse> {
-  return {
-    status: "error",
-    message: "Not implemented.",
-  };
+  const { userId, role } = decodeJwt(headers.Authorization);
+  const apartment = await connection.manager.getRepository(Apartment).findOne({
+    apartmentId,
+  });
+  if (!apartment) {
+    return {
+      status: "error",
+      message: "No such apartment.",
+    };
+  }
+  switch (role) {
+    case "client":
+      return {
+        status: "error",
+        message: "Clients cannot delete apartment listings.",
+      };
+    case "realtor":
+      if (apartment.realtor.userId !== userId) {
+        return {
+          status: "error",
+          message: "Realtors cannot delete other realtors' apartment listings.",
+        };
+      }
+      await connection.manager.getRepository(Apartment).delete({
+        apartmentId,
+        realtor: {
+          userId,
+        },
+      });
+      // TODO: Check deleted count.
+      return {
+        status: "success",
+        message: "The apartment listing was deleted successfully.",
+      };
+    case "admin":
+      await connection.manager.getRepository(Apartment).delete({
+        apartmentId,
+      });
+      // TODO: Check deleted count.
+      return {
+        status: "success",
+        message: "The apartment listing was deleted successfully.",
+      };
+    default:
+      throw new Error(`Unknown role: ${role}.`);
+  }
 }
---
+++ server/src/endpoints/scenarios/apartments/delete.spec.ts
@@ -0,0 +1,109 @@
+import { ApartmentInfo } from "@/api";
+import { connection } from "@/db/connections";
+import { Apartment } from "@/db/entities/apartment";
+import { deleteApartment } from "@/endpoints/deleteApartment";
+import { useTestingDatabase } from "@/testing/db";
+import { setUpJwtForTesting } from "@/testing/jwt";
+import {
+  ADMIN_FRANK,
+  authHeaders,
+  BRIAN_PASSWORD,
+  CLIENT_BRIAN,
+  createTestUsers,
+  findUser,
+  FRANK_PASSWORD,
+  HELENA_PASSWORD,
+  JOHN_PASSWORD,
+  REALTOR_HELENA,
+  REALTOR_JOHN,
+} from "@/testing/users";
+import "jest";
+
+const APARTMENT_INFO: ApartmentInfo = {
+  floorArea: 20,
+  pricePerMonth: 1000,
+  numberOfRooms: 1,
+  coordinates: {
+    latitude: 20,
+    longitude: 30,
+  },
+  rented: false,
+};
+
+setUpJwtForTesting();
+useTestingDatabase();
+
+beforeEach(async () => {
+  await createTestUsers();
+});
+
+test("clients cannot delete apartment listings", async () => {
+  const realtor = await findUser(REALTOR_HELENA);
+  const apartment = Apartment.create(APARTMENT_INFO, realtor.userId);
+  await connection.manager.save(apartment);
+
+  const response = await deleteApartment(
+    await authHeaders(CLIENT_BRIAN, BRIAN_PASSWORD),
+    apartment.apartmentId,
+  );
+  expect(response).toMatchObject({
+    status: "error",
+    message: "Clients cannot delete apartment listings.",
+  });
+});
+
+test("realtors can delete their own apartment listings", async () => {
+  const realtor = await findUser(REALTOR_HELENA);
+  const apartment = Apartment.create(APARTMENT_INFO, realtor.userId);
+  await connection.manager.save(apartment);
+
+  const response = await deleteApartment(
+    await authHeaders(REALTOR_HELENA, HELENA_PASSWORD),
+    apartment.apartmentId,
+  );
+  expect(response).toMatchObject({
+    status: "success",
+    message: "The apartment listing was deleted successfully.",
+  });
+});
+
+test("realtors cannot delete other realtors' apartment listings", async () => {
+  const realtor = await findUser(REALTOR_HELENA);
+  const apartment = Apartment.create(APARTMENT_INFO, realtor.userId);
+  await connection.manager.save(apartment);
+
+  const response = await deleteApartment(
+    await authHeaders(REALTOR_JOHN, JOHN_PASSWORD),
+    apartment.apartmentId,
+  );
+  expect(response).toMatchObject({
+    status: "error",
+    message: "Realtors cannot delete other realtors' apartment listings.",
+  });
+});
+
+test("admins can any realtor's apartment listings", async () => {
+  const realtor = await findUser(REALTOR_HELENA);
+  const apartment = Apartment.create(APARTMENT_INFO, realtor.userId);
+  await connection.manager.save(apartment);
+
+  const response = await deleteApartment(
+    await authHeaders(ADMIN_FRANK, FRANK_PASSWORD),
+    apartment.apartmentId,
+  );
+  expect(response).toMatchObject({
+    status: "success",
+    message: "The apartment listing was deleted successfully.",
+  });
+});
+
+test("missing apartment listings raise error", async () => {
+  const response = await deleteApartment(
+    await authHeaders(REALTOR_HELENA, HELENA_PASSWORD),
+    "missing",
+  );
+  expect(response).toMatchObject({
+    status: "error",
+    message: "No such apartment.",
+  });
+});
