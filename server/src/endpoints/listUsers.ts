e5137a034b58e3587f44db60a70829856b7a0a87
Francois Wouts <f@zenc.io>
2018-04-04 17:41:23 +1000

    Set up user listing endpoint

--- server/src/endpoints/listUsers.ts
+++ server/src/endpoints/listUsers.ts
@@ -1,11 +1,72 @@
-import { AuthRequired, ListUsersRequest, ListUsersResponse } from "../api";
+import { decodeJwt } from "@/auth/jwt";
+import { connection } from "@/db/connections";
+import { User } from "@/db/entities/user";
+import {
+  AuthRequired,
+  ListUsersRequest,
+  ListUsersResponse,
+  UserDetails,
+} from "../api";
+
+const MAX_RESULTS_PER_PAGE = 100;
 
 export async function listUsers(
   headers: AuthRequired,
   request: ListUsersRequest,
+  maxResultsPerPage = MAX_RESULTS_PER_PAGE,
 ): Promise<ListUsersResponse> {
+  let role = "unknown";
+  try {
+    ({ role } = decodeJwt(headers.Authorization));
+  } catch {
+    // Ignore.
+  }
+  if (role !== "admin") {
+    return {
+      results: [],
+      totalResults: 0,
+    };
+  }
+  let skip;
+  if (request.pageToken) {
+    try {
+      const token = JSON.parse(atob(request.pageToken));
+      skip = parseInt(token.skip, 10);
+    } catch (e) {
+      // Ignore any error, but log it for debugging purposes.
+      // tslint:disable-next-line no-console
+      console.warn(e);
+      skip = 0;
+    }
+  } else {
+    skip = 0;
+  }
+  const [results, totalCount] = await connection.manager.findAndCount(User, {
+    where: {
+      ...(request.filter && {
+        role: request.filter.role,
+      }),
+    },
+    skip,
+    take: maxResultsPerPage,
+  });
   return {
-    results: [],
-    totalResults: 0,
+    results: results.map(toUserDetails),
+    totalResults: totalCount,
+    ...(skip + results.length < totalCount && {
+      nextPageToken: btoa(
+        JSON.stringify({
+          skip: skip + maxResultsPerPage,
+        }),
+      ),
+    }),
+  };
+}
+
+function toUserDetails(user: User): UserDetails {
+  return {
+    userId: user.userId,
+    email: user.email,
+    role: user.role,
   };
 }
---
+++ server/src/endpoints/scenarios/users/list.spec.ts
@@ -0,0 +1,117 @@
+import { listUsers } from "@/endpoints/listUsers";
+import { useTestingDatabase } from "@/testing/db";
+import { setUpJwtForTesting } from "@/testing/jwt";
+import {
+  ADMIN_FRANK,
+  authHeaders,
+  BRIAN_PASSWORD,
+  CLIENT_BRIAN,
+  createTestUsers,
+  FRANK_PASSWORD,
+  HELENA_PASSWORD,
+  REALTOR_HELENA,
+} from "@/testing/users";
+import "jest";
+
+setUpJwtForTesting();
+useTestingDatabase();
+
+beforeEach(async () => {
+  await createTestUsers();
+});
+
+test("non-admins cannot list users", async () => {
+  expect(
+    await listUsers(
+      {
+        Authorization: "",
+      },
+      {},
+    ),
+  ).toEqual({
+    results: [],
+    totalResults: 0,
+  });
+  expect(
+    await listUsers(await authHeaders(CLIENT_BRIAN, BRIAN_PASSWORD), {}),
+  ).toEqual({
+    results: [],
+    totalResults: 0,
+  });
+  expect(
+    await listUsers(await authHeaders(REALTOR_HELENA, HELENA_PASSWORD), {}),
+  ).toEqual({
+    results: [],
+    totalResults: 0,
+  });
+});
+
+test("admins can list users", async () => {
+  const response = await listUsers(
+    await authHeaders(ADMIN_FRANK, FRANK_PASSWORD),
+    {},
+  );
+  expect(response.results.length).toBe(6);
+  expect(response.totalResults).toBe(6);
+  expect(response.nextPageToken).toBeUndefined();
+});
+
+test("users filtering", async () => {
+  const clients = await listUsers(
+    await authHeaders(ADMIN_FRANK, FRANK_PASSWORD),
+    {
+      filter: {
+        role: "client",
+      },
+    },
+  );
+  expect(clients.results.length).toBe(2);
+  expect(clients.results.map((u) => u.role)).toEqual(["client", "client"]);
+  expect(clients.totalResults).toBe(2);
+
+  const realtors = await listUsers(
+    await authHeaders(ADMIN_FRANK, FRANK_PASSWORD),
+    {
+      filter: {
+        role: "realtor",
+      },
+    },
+  );
+  expect(realtors.results.length).toBe(2);
+  expect(realtors.results.map((u) => u.role)).toEqual(["realtor", "realtor"]);
+  expect(realtors.totalResults).toBe(2);
+
+  const admins = await listUsers(
+    await authHeaders(ADMIN_FRANK, FRANK_PASSWORD),
+    {
+      filter: {
+        role: "admin",
+      },
+    },
+  );
+  expect(admins.results.length).toBe(2);
+  expect(admins.results.map((u) => u.role)).toEqual(["admin", "admin"]);
+  expect(admins.totalResults).toBe(2);
+});
+
+test("users pagination", async () => {
+  const usersPerPage = 3;
+  const page1 = await listUsers(
+    await authHeaders(ADMIN_FRANK, FRANK_PASSWORD),
+    {},
+    usersPerPage,
+  );
+  expect(page1.results.length).toBe(3);
+  expect(page1.totalResults).toBe(6);
+  expect(page1.nextPageToken).toBeTruthy();
+  const page2 = await listUsers(
+    await authHeaders(ADMIN_FRANK, FRANK_PASSWORD),
+    {
+      pageToken: page1.nextPageToken,
+    },
+    usersPerPage,
+  );
+  expect(page2.results.length).toBe(3);
+  expect(page2.totalResults).toBe(6);
+  expect(page2.nextPageToken).toBeUndefined();
+});
