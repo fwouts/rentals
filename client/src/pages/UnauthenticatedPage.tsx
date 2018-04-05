fcca9e93b7fa3ff251103e5a576b0ff9c9ae8367
Francois Wouts <f@zenc.io>
2018-04-05 14:45:00 +1000

    Remove unused CSS for now

--- client/src/pages/UnauthenticatedPage.tsx
+++ client/src/pages/UnauthenticatedPage.tsx
@@ -2,7 +2,6 @@ import { Button } from "element-react";
 import { observer } from "mobx-react";
 import * as React from "react";
 import { Unauthenticated } from "../state/unauthenticated";
-import "./UnauthenticatedPage.scss";
 
 @observer
 export class UnauthenticatedPage extends React.Component<{controller: Unauthenticated}> {
