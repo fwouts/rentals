4a823c4233987051d95ac04bf360acf833ae68a5
Francois Wouts <f@zenc.io>
2018-04-05 17:56:55 +1000

    Show apartment listings in admin

--- client/src/pages/AuthenticatedAdminPage.tsx
+++ client/src/pages/AuthenticatedAdminPage.tsx
@@ -1,19 +1,30 @@
 import { Menu } from "element-react";
 import { observer } from "mobx-react";
 import * as React from "react";
+import { ListingApartmentsComponent } from "../components/apartments/ListingApartmentsComponent";
 import { AuthenticatedAdmin } from "../state/authenticated/admin";
 
 @observer
 export class AuthenticatedAdminPage extends React.Component<{controller: AuthenticatedAdmin}> {
   public render() {
+    let element;
+    switch (this.props.controller.state.kind) {
+      case "listing-apartments":
+        element = <ListingApartmentsComponent controller={this.props.controller.state} />;
+        break;
+    }
     return (
       <div>
-        <Menu theme="dark" onSelect={this.onMenuSelect}>
+        <Menu theme="dark" mode="horizontal" onSelect={this.onMenuSelect}>
+          <Menu.SubMenu index="apartments" title="Apartments">
+            <Menu.Item index="apartments-list">Browse apartments</Menu.Item>
+            <Menu.Item index="apartments-create">Create an apartment listing</Menu.Item>
+          </Menu.SubMenu>
           <Menu.Item index="signout">
             Sign out
           </Menu.Item>
         </Menu>
-        Hi admin
+        {element}
       </div>
     );
   }
@@ -22,6 +33,9 @@ export class AuthenticatedAdminPage extends React.Component<{controller: Authent
     switch (index) {
       case "signout":
         this.props.controller.signOut();
+        break;
+      case "apartments-list":
+        this.props.controller.listApartments();
         break;
     }
   }
